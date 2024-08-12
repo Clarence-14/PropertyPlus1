21a05e2c699be395642e1606ea4d07f9569f0659
Clarence-14 <thisisclarence1@gmail.com>
2024-08-12 09:08:56 +0300

    commit

---
+++ app/src/main/java/com/example/propertyplus/navigation/AppNavHost.kt
@@ -0,0 +1,53 @@
+package com.example.propertyplus.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.propertyplus.ui.theme.screens.about.AboutScreen
+import com.example.propertyplus.ui.theme.screens.details.DetailsScreen
+import com.example.propertyplus.ui.theme.screens.home.HomeScreen
+import com.example.propertyplus.ui.theme.screens.intent.IntentScreen
+import com.example.propertyplus.ui.theme.screens.property.PropertyScreen
+import com.example.propertyplus.ui.theme.screens.splash.SplashScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination:String = ROUT_SPLASH
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+
+        composable(ROUT_HOME) {
+            HomeScreen(navController = navController)
+        }
+
+        composable(ROUT_DETAIL) {
+            DetailsScreen(navController = navController)
+        }
+
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController = navController)
+        }
+
+        composable(ROUT_SPLASH) {
+            SplashScreen(navController = navController)
+        }
+
+        composable(ROUT_PROPERTY) {
+            PropertyScreen(navController = navController)
+        }
+
+        composable(ROUT_INTENT) {
+            IntentScreen(navController = navController)
+        }
+
+    }
+}
